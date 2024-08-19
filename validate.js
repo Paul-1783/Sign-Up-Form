e8b5da75de114c228bfdfc28d54c3f0cd3f059bf
Paul-1783 <paulschmitt144@gmail.com>
2024-08-19 17:21:00 +0200

    made password checks congruent

--- validate.js
+++ validate.js
@@ -1,16 +1,49 @@
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#pw_confirm");
 const button = document.querySelector("input[type=button]");
-console.log("tad  ");
+const error = document.querySelector("span.error");
+const errorConfirm = document.querySelector("span.errorConfirm");
 
-button.addEventListener("check", (event) => {
+password.addEventListener("input", (event) => {
   if (password.value !== confirmPassword.value) {
-    console.log(password.value, "   wefewf  ", confirmPassword.value);
-    confirmPassword.setCustomValidity(
-      "Passwort nicht übereinstimmend mit erster Eingabe"
-    );
+    console.log(password.value, "   in password  ", confirmPassword.value);
+    error.textContent = "* Password and Confirmation need to be congruent.";
+    error.className = "error active";
+    event.preventDefault();
+  } else if (
+    password.validity.valueMissing ||
+    confirmPassword.validity.valueMissing
+  ) {
+    error.textContent = "* You need to enter a password and a confirmation.";
+    error.className = "error active";
+    event.preventDefault();
   } else {
-    confirmPassword.setCustomValidity("");
-    console.log("dvwvrwv");
+    error.textContent = "";
+    error.className = "error";
+    errorConfirm.textContent = "";
+    errorConfirm.className = "error";
+  }
+});
+
+confirmPassword.addEventListener("input", (event) => {
+  if (password.value !== confirmPassword.value) {
+    console.log(password.value, "  in confirm  ", confirmPassword.value);
+    errorConfirm.textContent =
+      "* Password and Confirmation need to be congruent.";
+    errorConfirm.className = "error active";
+    event.preventDefault();
+  } else if (
+    password.validity.valueMissing ||
+    confirmPassword.validity.valueMissing
+  ) {
+    errorConfirm.textContent =
+      "* You need to enter a password and a confirmation.";
+    event.preventDefault();
+    errorConfirm.className = "error active";
+  } else {
+    errorConfirm.className = "error";
+    errorConfirm.className = "error";
+    error.className = "error";
+    error.textContent = "";
   }
 });
